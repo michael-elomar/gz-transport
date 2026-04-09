98217d4c1b53f9686e23cc0ab4d9a1c1f94048c9
Michael El Omar <michaelelomar@outlook.com>
2026-04-09 20:33:00 +0200

    set export flags in atom.mk

--- atom.mk
+++ atom.mk
@@ -15,18 +15,16 @@ LOCAL_CXXFLAGS := \
 	-Wno-deprecated-declarations \
 	-Wno-deprecated-copy
 
-# LOCAL_ARCHIVE_PATCHES = homebrew.patch warnings.patch
-
 LOCAL_LIBRARIES := \
 	gz-cmake
 
 LOCAL_CMAKE_CONFIGURE_ARGS := \
 	-DBUILD_TESTING:BOOL=False
 
-# LOCAL_EXPORT_C_INCLUDES := $(TARGET_OUT_STAGING)/usr/include/ignition/transport8
+LOCAL_EXPORT_C_INCLUDES := $(TARGET_OUT_STAGING)/usr/include/gz/transport15
 
-# LOCAL_EXPORT_LDLIBS := \
-#	-lignition-transport8
+LOCAL_EXPORT_LDLIBS := \
+	-lgz-transport15
 
 include $(BUILD_CMAKE)
 
